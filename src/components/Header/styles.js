6853c9950b73696e046e37e9919e58881c877689
Carlos Bruno <carlao.linux@gmail.com>
2020-01-11 22:33:47 -0300

    Acrescentando Header | logo

---
+++ src/components/Header/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px 0;
+
+  img {
+    width: 70px;
+  }
+
+`;
